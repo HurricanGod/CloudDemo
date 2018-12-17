b836567872e97e6c7e1c236132f842d38c45c68b
HurricanGod <1807363937@qq.com>
2018-12-17 21:21:56 +0800

    Initial commit

---
+++ eurekaserver/src/test/java/cn/hurrican/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.hurrican.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
