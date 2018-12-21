d0885a9b836ab675aa18ebe544d642789580f5c1
HurricanGod <1807363937@qq.com>
2018-12-21 19:34:26 +0800

    submit code

--- consumerribbon/src/main/java/cn/hurrican/consumerribbon/controller/TestController.java
+++ consumerribbon/src/main/java/cn/hurrican/consumerribbon/controller/TestController.java
@@ -1,6 +1,5 @@
 package cn.hurrican.consumerribbon.controller;
 
-import cn.hurrican.model.MySqlConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * 测试RPC调用
+ *
+ * @author Hurrican
+ * @Date 2018/12/21  9:37
+ * @since 1.0.0
+ */
 
 @RestController
 public class TestController {
@@ -19,11 +28,15 @@ public class TestController {
     @RequestMapping(value = "/getConfig")
     public String getConfig() {
         String url = "http://SERVICE-CLIENT/queryConfig";
-        ResponseEntity<MySqlConfig> entity = restTemplate.getForEntity(url, MySqlConfig.class);
-        MySqlConfig body = entity.getBody();
+        ResponseEntity<List> responseEntity = restTemplate.getForEntity(url, List.class);
+        List list = responseEntity.getBody();
+        if (list == null) {
+            return "";
+        }
+        list.forEach(System.out::println);
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.writeValueAsString(body);
+            return objectMapper.writeValueAsString(list);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             return e.getMessage();
