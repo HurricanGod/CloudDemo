fc27955635adf74c29ebae83b2776cf758a712b1
HurricanGod <1807363937@qq.com>
2018-12-27 18:31:44 +0800

    submit code

--- eureka-server-cluster/src/main/java/cn/hurrican/eurekacluster/EurekaServerClusterApplication.java
+++ eureka-server-cluster/src/main/java/cn/hurrican/eurekacluster/EurekaServerClusterApplication.java
@@ -2,8 +2,18 @@ package cn.hurrican.eurekacluster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server 集群
+ *
+ * @author Hurrican
+ * @Date 2018/12/27  9:48
+ * @since 1.0.0
+ */
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerClusterApplication {
 
     public static void main(String[] args) {
--- eurekaserver/src/main/java/cn/hurrican/service/impl/AppConfigServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package cn.hurrican.service.impl;
-
-import cn.hurrican.service.AppConfigService;
-
-public class AppConfigServiceImpl implements AppConfigService {
-}
