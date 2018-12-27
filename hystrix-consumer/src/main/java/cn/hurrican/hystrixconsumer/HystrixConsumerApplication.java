97bb79c4657277418250d1e0908888bfdb22a93d
HurricanGod <1807363937@qq.com>
2018-12-28 00:37:16 +0800

    submit code

---
+++ hystrix-consumer/src/main/java/cn/hurrican/hystrixconsumer/HystrixConsumerApplication.java
@@ -0,0 +1,14 @@
+package cn.hurrican.hystrixconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixConsumerApplication.class, args);
+    }
+
+}
+
