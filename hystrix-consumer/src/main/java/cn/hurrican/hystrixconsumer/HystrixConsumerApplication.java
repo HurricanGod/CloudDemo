9fbdf2dd4194d0bc857995e41d4dc7f2730b0617
HurricanGod <1807363937@qq.com>
2018-12-28 19:27:59 +0800

    submit code

--- hystrix-consumer/src/main/java/cn/hurrican/hystrixconsumer/HystrixConsumerApplication.java
+++ hystrix-consumer/src/main/java/cn/hurrican/hystrixconsumer/HystrixConsumerApplication.java
@@ -3,6 +3,14 @@ package cn.hurrican.hystrixconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 断路器启动类
+ *
+ * @author Hurrican
+ * @Date 2018/12/28  10:23
+ * @since 1.0.0
+ */
+
 @SpringBootApplication
 public class HystrixConsumerApplication {
 
